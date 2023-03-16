3d98cb0b062765eb40e5f99d988bb504b75c0eb5
Sneha <snehalall.sneha@gmail.com>
2023-03-16 21:53:48 +0530

    feat:add test coverage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header({ text }) {
+  const navigate = useNavigate();
   return (
     <div className="header-comp">
       <p>{text}</p>
-      <div className='logout'><FontAwesomeIcon icon={ faUser}/></div>
+      <div className='logout' onClick={()=>navigate('/login')}><FontAwesomeIcon icon={ faUser}/></div>
     </div>
   );
 }
